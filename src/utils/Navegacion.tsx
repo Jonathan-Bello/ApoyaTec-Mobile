c7b9d3ff4c2bba5f6cca81824adab6b2710ca87e
Jonathan Bello <jonathan.bellofg@gmail.com>
2022-06-01 04:49:01 -0500

    created bottom bar and top bar

--- src/utils/Navegacion.tsx
+++ src/utils/Navegacion.tsx
@@ -3,25 +3,28 @@ import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './../screens/Login';
 import UserContext from '../context/userContext';
 import Dashboard from './../screens/Dashboard';
 import Account from './../screens/Account';
 import Course from '../screens/Course';
 import { RootStackParamList } from './RootStackParam';
-
+import Login from '../screens/templates/Login';
 
 const Navegacion = () => {
   const Stack = createStackNavigator<RootStackParamList>();
   return (
     <NavigationContainer>
       {/* <UserContext> */}
-        <Stack.Navigator >
-          <Stack.Screen name='Login' component={Login} />
-          <Stack.Screen name='Dashboard' component={Dashboard} />
-          {/* <Stack.Screen name='Course/:id' component={Course} />
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+        {/* <Stack.Screen name='Course/:id' component={Course} />
           <Stack.Screen name='Account' component={Account} /> */}
-        </Stack.Navigator>
+      </Stack.Navigator>
       {/* </UserContext> */}
     </NavigationContainer>
   );

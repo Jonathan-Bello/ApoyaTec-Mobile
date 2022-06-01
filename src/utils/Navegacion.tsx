81500136c5c322d6252f69f79913c99c1d8f90c9
AngelGM <cesargasca083@gmail.com>
2022-05-31 22:58:56 -0500

    Cambios para evitar problemas de compatibilidad

---
+++ src/utils/Navegacion.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Navegacion = () => {
+  return (
+    <View>
+      <Text>Navegacion</Text>
+    </View>
+  );
+};
+
+export default Navegacion;

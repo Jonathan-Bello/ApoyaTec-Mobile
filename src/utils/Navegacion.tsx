4b80913ddb7e71ecd70388ca3dd90f75345c5e9e
AngelGM <cesargasca083@gmail.com>
2022-05-31 23:04:13 -0500

    Primeras rutas

--- src/utils/Navegacion.tsx
+++ src/utils/Navegacion.tsx
@@ -1,11 +1,19 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './../screens/Login';
+
+
 const Navegacion = () => {
+  const Stack = createStackNavigator();
   return (
-    <View>
-      <Text>Navegacion</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={Login}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 

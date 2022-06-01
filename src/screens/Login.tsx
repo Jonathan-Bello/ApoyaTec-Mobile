c4c768b47f48287bdb7f055a987e2a467100ab66
AngelGM <cesargasca083@gmail.com>
2022-05-31 23:43:30 -0500

    Correccion de compatibilidad

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { Formik } from 'formik';
 
 const Login = () => {
+
+  const formik = Formik.toString()
   return (
     <View>
       <Text>Login</Text>

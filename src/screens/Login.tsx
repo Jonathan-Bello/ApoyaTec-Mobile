c5f17d7cc085270e44c2b83178ae8d60aafde778
Jonathan Bello <jonathan.bellofg@gmail.com>
2022-06-01 05:16:20 -0500

    Created login maqueta

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,26 +1,27 @@
-import { View, Text, Pressable } from 'react-native';
+import { View, Text, Pressable, Image, StyleSheet } from 'react-native';
 import React, { useContext, useState, useEffect } from 'react';
 import Appbar from './../components/appbar/Appbar';
-import { useFormik } from 'formik';
+import { Formik, useFormik } from 'formik';
 import { Contexto } from '../context/userContext';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../utils/RootStackParam';
 import Axios from './../../node_modules/axios/index.d';
 import * as Yup from 'yup';
 import { Credentials } from '../models/Credentials';
+import { Button, Input } from '@rneui/base';
 
 type Props = StackScreenProps<RootStackParamList, 'Login'>;
 
 const Login = ({ navigation }: Props) => {
   const context = useContext(Contexto);
-  
+
   const [usuario, setUsuario] = useState<Credentials>();
   useEffect(() => {
-    const nuevaCredecial : Credentials= {
-      jwt : "Este es un jwt",
-      id: 10
-    }
-    setUsuario(nuevaCredecial)
+    const nuevaCredecial: Credentials = {
+      jwt: 'Este es un jwt',
+      id: 10,
+    };
+    setUsuario(nuevaCredecial);
   }, []);
 
   // const validaciones = Yup.object({
@@ -52,15 +53,37 @@ const Login = ({ navigation }: Props) => {
   return (
     <View>
       <Appbar />
-      <Text>Login</Text>
-      <Pressable onPress={() => {
-        context.IniciarSesion(usuario!); 
-          navigation.navigate('Dashboard')
-        // console.log("first");
-        }}
-          ><Text>Ir al Dashboard</Text></Pressable>
+      <View style={styles.container}>
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <Image
+            style={styles.img}
+            source={require('../assets/img/Mapi.png')}
+          />
+        </View>
+
+        <View>
+          <Input
+            keyboardType="email-address"
+            placeholder="emial@email.com"></Input>
+          <Input keyboardType="visible-password" placeholder="********"></Input>
+          <Button>Ingresar</Button>
+        </View>
+      </View>
     </View>
   );
 };
 
-export default Login
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  img: {
+    width: '50%',
+    height: '50%',
+  },
+});
